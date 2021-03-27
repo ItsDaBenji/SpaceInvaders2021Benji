851ca3228ca60afc22177c585908def20107c144
Benjamin Blackmon <benmblackmon@gmail.com>
2021-03-26 23:57:17 -0500

    Initial commit

---
+++ Assets/TouchControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour
+{
+
+    [SerializeField] float speed;
+    
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 15.66f));
+                transform.position = Vector3.Lerp(transform.position, touchPosition, speed * Time.deltaTime);
+            }
+        }
+    }
+
+}
